3804046a00d78f13fe8ee5605e037535d40b6d64
Ronal Daniel Maita Vasquez <daniel_ronald17@hotmail.com>
2012-10-24 15:42:27 -0500

    creacion de index php en carpeta ronal
    creacion de index php en carpeta ronal para nuevo index

---
+++ ronal/index.php
@@ -0,0 +1,38 @@
+<?php
+$script_slidersup=true;
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+    <meta property="og:type" content="website">
+    <meta property="og:site_name" content="Instituto de Educación Superior Tecnológico Privado Amauta">
+    <meta property="og:title" content="Instituto de Educación Superior Tecnológico Privado Amauta">
+    <meta property="og:description" content="Formamos profesionales técnicos en Computación e Informática, Gestión Empresarial (Administración y Marketing) con calidad académica y administrativa, generando creatividad y mentalidad emprendedora empresarial.">
+    <meta property="og:url" content="http://istpamauta.edu.pe">
+	<title>Instituto de Educación Superior Tecnnológico Privado Amauta</title>
+    
+    <?php require_once("header-scripts.php"); ?>
+    
+</head>
+
+<body>
+
+<?php require_once("header.php"); ?>
+
+<section>
+	<div class="interior">
+    	
+        <h2>Bienvenidos</h2>
+        
+        <p>Quisiéramos saludarte y a travéz de la presente darte la bienvenida al instituto privado AMAUTA, agradeciéndote a ti de manera anticipada y a tu familia por deporsitarnos su confianza para formarte en un carrera profesional.</p>
+
+<p>Como sabrás en el presente siglo xxI, el competitivo mercado laboral nos exíge una capacitación contínua y una sólida formación profesional ya que los conocimientos van cambiando con una rapidez increible y para ello debes desarrollar competencias como el: pensamiento critico, la iniciativa, el pensamiento sistématico y estratégico, dominio de relaciones interpersonales y la capacidad para tomar decisiones frente a escenarios de incertidumbre.</p>
+        
+    </div>
+</section><!-- FIN SECTION -->
+
+<?php require_once("footer.php"); ?>
+
+</body>
+</html>
