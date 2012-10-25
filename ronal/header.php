80210ac4d0b0da77facc4eb482d3329c4345553c
Ronal Daniel Maita Vasquez <daniel_ronald17@hotmail.com>
2012-10-25 15:21:22 -0500

    footer, header-scripts, header en ronal
    creacionde estos tres archivos para realizar practicas

---
+++ ronal/header.php
@@ -0,0 +1,70 @@
+<header class="limpiar">
+	
+    <div class="interior limpiar">
+    	
+        <div id="header_contenido" class="limpiar">
+        
+            <div id="header_sup" class="limpiar">
+            	<div id="cnt_logo">
+                <h1><a href="#" id="logo_sup">Instituto Superior Tecnológico Privado Amauta</a></h1>
+                </div>
+                
+                <div id="wg_social">
+                    <ul id="wgsc_items">
+                        <li><a target="_blank" class="wgsc_facebook" href="http://facebook.com/amauta.istp">Facebook</a></li>
+                        <li><a target="_blank" class="wgsc_twitter" href="http://twitter.com/istp_amauta">Twitter</a></li>
+                    </ul>
+                </div>
+                
+            </div><!-- FIN HEADER SUP -->
+                    
+            <nav>
+                <ul id="nav" class="sf-menu">
+                    <li class="first bgr"><a class="mains" href="/" title="">Inicio</a></li>
+                    <li class="bgr"><a class="mains" href="nosotros.php" title="">Nosotros</a></li>
+                    <li class="bgr"><a class="mains" href="javascript:;" title="">Carreras Profesionales</a>
+                    	<ul class="submenu">
+                            <li><a href="carrera-adm.php">Administración de Empresas</a></li>
+                            <li><a href="carrera-cmp.php">Computación e Informática</a></li>
+                            <li><a href="carrera-gst.php">Gastronomia</a></li>
+                            <li><a href="carrera-mkt.php">Marketing Empresarial</a></li>
+                        </ul>
+                    </li>
+                    <li class="bgr"><a class="mains" href="#" title="">Centro de Emprendimiento</a>
+                    	<ul class="submenu">
+                            <li><a href="empr-chef.php">Chef de Alta Cocina</a></li>
+                            <li><a href="empr-compu.php">Computación e Informática</a></li>
+                            <li><a href="empr-gastro.php">Gastronomia y Arte Culinario</a></li>
+                            <li><a href="empr-markt.php">Marketing Empresarial</a></li>
+                        </ul>
+                    </li>
+                    <li class="bgr"><a class="mains" href="idiomas.php" title="">Centro de Idiomas</a></li>
+                    <li class="bgr"><a class="mains" href="sedes.php" title="">Sedes</a></li>
+                    <li class="last bgr"><a class="mains last" href="#" title="">Contactenos</a></li>
+                </ul><!-- FIN MENU -->
+            </nav><!-- FIN NAV -->
+            
+            <?php if($script_slidersup==true){ ?>
+            <div id="slide_superior">
+            	<div id="slsup_items">
+                	<a href="carrera-adm.php">
+           	  			<img src="imagenes/slide/amauta1.jpg" alt="Imagen" width="940" height="415" />
+                    </a>
+                    <a href="carrera-cmp.php">
+           	  			<img src="imagenes/slide/amauta2.jpg" alt="Imagen" width="940" height="415" />
+                    </a>
+                    <a href="carrera-gst.php">
+           	  			<img src="imagenes/slide/amauta3.jpg" alt="Imagen" width="940" height="415" />
+                    </a>
+                    <a href="carrera-mkt.php">
+           	  			<img src="imagenes/slide/amauta4.jpg" alt="Imagen" width="940" height="415" />
+                    </a>
+                </div>
+            </div>
+            <?php } ?>
+        
+        </div><!-- FIN CONTENIDO HEADER -->
+        
+    </div><!-- FIN INTERIOR -->
+    
+</header><!-- FIN HEADER -->
