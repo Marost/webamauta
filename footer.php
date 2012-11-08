bf3c3f33b62a57b678ea1a430d4e870e2bcc7f82
Ronal Daniel Maita Vasquez <daniel_ronald17@hotmail.com>
2012-11-08 15:23:35 -0500

    cambio de telefonos y elminicacion de un archivo
    actulizacionde los numeros telefonicos y eliminacionde un archivo que no
    se usa para ahorrar espacio

--- footer.php
+++ footer.php
@@ -4,9 +4,9 @@
         <div class="ft_datos">
         	
             <h4>Sedes</h4>
-            <p><span>Comas:</span> Av. Túpac Amaru 809, Urb. Huaquillay  (Frente al paradero Jorge Chavez) |   5251707 / 7661944</p>
-            <p><span>Los Olivos:</span> Jr. Salaverry 3606 con Sta. Cruz de Pachacutec (Frente a Megaplaza costado del Banco de la Nación) |   3686799</p>
-            <p><span>Puente Piedra:</span> Av. República de Venezuela 258 (Frente a la Municipalidad de Puente Piedra) | 7352323 / 5566656</p>
+            <p><span>Comas:</span> Av. Túpac Amaru 809, Urb. Huaquillay  (Frente al paradero Jorge Chavez) |   5251707</p>
+            <p><span>Los Olivos:</span> Jr. Salaverry 3606 con Sta. Cruz de Pachacutec (Frente a Megaplaza costado del Banco de la Nación) |    3866799</p>
+            <p><span>Puente Piedra:</span> Av. República de Venezuela 258 (Frente a la Municipalidad de Puente Piedra) | 7355328</p>
             
         </div>
 	</div>
